07df4ff21194ac23fff73a7e79b8e38b69aa9ecc
Antonio Jimenez <antonio.jaguilera2@gmail.com>
2024-05-08 15:43:45 +0200

    segundo commit

---
+++ api/api/src/main/java/com/example/apibuceo/api/models/Salidas.java
@@ -0,0 +1,8 @@
+package com.example.apibuceo.api.models;
+
+public class Salidas {
+    private String fecha;
+    private String hora;
+    private int capacidad;
+    private int plazasDisponibles;
+}
