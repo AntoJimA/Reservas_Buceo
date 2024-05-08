7fdd82a69b2e3148dc028db3740c2f681124c0fb
Antonio Jimenez <antonio.jaguilera2@gmail.com>
2024-05-08 15:33:29 +0200

    primer commit

---
+++ api/api/src/main/java/com/example/apibuceo/api/models/Usuario.java
@@ -0,0 +1,46 @@
+package com.example.apibuceo.api.models;
+public class Usuario{
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String nivelBuceo;
+
+    public Usuario(String nombre, String apellido, String email, String nivelBuceo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.nivelBuceo = nivelBuceo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNivelBuceo() {
+        return nivelBuceo;
+    }
+
+    public void setNivelBuceo(String nivelBuceo) {
+        this.nivelBuceo = nivelBuceo;
+    }
+}
