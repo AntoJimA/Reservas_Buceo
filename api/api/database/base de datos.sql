15175cbdc777194a7bc1c608d8f43494a6c6598f
Antonio Jimenez <antonio.jaguilera2@gmail.com>
2024-05-11 17:13:12 +0200

    base de datos hecha

--- api/api/database/base de datos.sql
+++ api/api/database/base de datos.sql
@@ -1,17 +1,30 @@
 DROP database if exists usuario;
 create database usuario;
 use usuario;
-create table usuario(
+create table Usuario(
 id int not null unique auto_increment,
 nombre varchar(255),
 apellido varchar(255),
 email varchar(255),
-nivelBuceo varchar(255)
+nivelBuceo varchar(255),
+primary key(id)
 );
 
-create table salida(
+create table Salida(
+id int not null unique auto_increment,
 fecha date,
 hora char(5),
 capacidad int,
-plazasDisponiles int
+plazasDisponiles int,
+primary key(id)
 );
+
+create table Reserva(
+id_usuario int not null,
+id_salida int not null,
+primary key(id_usuario,id_salida),
+unique(id_usuario,id_salida),
+foreign key(id_usuario) references Usuario(id),
+foreign key(id_salida) references Salida(id) 
+);
+
